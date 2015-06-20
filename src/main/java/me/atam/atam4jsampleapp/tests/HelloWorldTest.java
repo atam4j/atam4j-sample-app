e3c2322426d1dc635ac071688e0574cf40e3e8c9
phillbarber <philliprbarber@googlemail.com>
2015-06-20 22:31:09 +0100

    Upraded sample application to version 0.4.0 of atam4j and added acceptance test.

--- src/main/java/me/atam/atam4jsampleapp/tests/HelloWorldTest.java
+++ src/main/java/me/atam/atam4jsampleapp/tests/HelloWorldTest.java
@@ -1,13 +1,15 @@
 package me.atam.atam4jsampleapp.tests;
 
+import me.atam.atam4j.Monitor;
 import me.atam.atam4j.configuration.ConfigLoader;
 import me.atam.atam4jsampleapp.configuration.TestConfiguration;
 import org.junit.Assert;
 import org.junit.Test;
 
+@Monitor
 public class HelloWorldTest {
 
-    TestConfiguration testConfig = new ConfigLoader<TestConfiguration>(TestConfiguration.class, "default-test-config.yml").getTestConfig();
+    TestConfiguration testConfig = new ConfigLoader<>(TestConfiguration.class, "default-test-config.yml").getTestConfig();
 
     @Test
     public void testHelloWorld() throws Exception {
